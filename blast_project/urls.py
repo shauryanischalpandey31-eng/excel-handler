69467341f9c65e185e6fe63c960b681d3dcb2c58
shauryanischalpandey31-eng <sudhafz25@gmail.com>
2025-11-30 14:03:32 +0530

    Add root URL redirect to /excel/

--- blast_project/urls.py
+++ blast_project/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('excel/', include('excel_handler.urls')),
+    path('', RedirectView.as_view(url='/excel/', permanent=False), name='home'),
 ]
 
 if settings.DEBUG:
